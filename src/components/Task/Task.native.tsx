0738dbdcf45614f0feecca655dc91dd4340e8686
Hussnain Ahmad <21100150@lums.edu.pk>
2021-08-30 15:40:20 +0500

    reverted back to default base config as babel was giving issues

---
+++ src/components/Task/Task.native.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const Task: React.FC = () => {
+  return (
+    <View>
+      <Text>Task 1</Text>
+    </View>
+  );
+};
+
+export default Task;
